03ac00fb1b1ceb7e8d03e57fff9fb2f9c630c9f8
myqway <myqway@outlook.com>
2016-04-09 17:57:53 +0800

    rebuild my blog style

--- source/mysite/blog/views.py
+++ source/mysite/blog/views.py
@@ -8,11 +8,11 @@ import json
 
 @blog.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('home.html')
 
 @blog.route('/home')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
 #https://pythonhosted.org/Flask-FlatPages/
 @blog.route('/scribbles')
@@ -20,20 +20,20 @@ def scribbles():
 	articles = (p for p in flatpages if 'date' in p.meta)
 	latest = sorted(articles, reverse=True,
 	                    key=lambda p: p.meta['time'])
-	return render_template('article.html', pages=latest)
+	return render_template('scribbles.html', pages=latest)
 
 @blog.route('/scribbles/<path:path>/')
-def scribble(path):
+def ascribble(path):
     page = flatpages.get_or_404(path)
-    return render_template('onepage.html', page=page)
+    return render_template('page.html', page=page)
 
 @blog.route('/resume')
 def resume():
 	return render_template('resume.html')
 
-@blog.route('/contact')
+@blog.route('/tags')
 def contact():
-	return "hello contact"
+	return render_template('tags.html')
 
 @blog.app_errorhandler(404)
 def page_not_found(error):
